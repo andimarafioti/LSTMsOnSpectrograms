c277b035ace6aad444df3b53b77fda0acc33fcf0
filsino <andimarafioti@gmail.com>
2018-05-21 18:06:09 +0200

    Simple architecture

--- architecture/simpleLSTMArchitecture.py
+++ architecture/simpleLSTMArchitecture.py
@@ -15,10 +15,10 @@ class SimpleLSTMArchitecture(Architecture):
 
 	def _lossGraph(self):
 		with tf.variable_scope("Loss"):
-			targetSquaredNorm = tf.reduce_sum(tf.square(self._target), axis=[1, 2, 3])
+			targetSquaredNorm = tf.reduce_sum(tf.square(self._target), axis=[1, 2])
 
 			error = self._target - self._output
-			error_per_example = tf.reduce_sum(tf.square(error), axis=[1, 2, 3])
+			error_per_example = tf.reduce_sum(tf.square(error), axis=[1, 2])
 
 			reconstruction_loss = 0.5 * tf.reduce_sum(error_per_example * (1 + 5 / (targetSquaredNorm + 1e-4)))
 			lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()]) * 1e-5
@@ -32,20 +32,26 @@ class SimpleLSTMArchitecture(Architecture):
 			return total_loss
 
 	def _network(self, data):
-		with tf.variable_scope("Network", reuse=not self._isTraining):
+		with tf.variable_scope("Network", reuse=False):
 			rnn_cell = tf.contrib.rnn.MultiRNNCell(
 				[tf.contrib.rnn.BasicLSTMCell(self._lstmParams.lstmSize()),
 				tf.contrib.rnn.BasicLSTMCell(self._lstmParams.lstmSize()),
 				tf.contrib.rnn.BasicLSTMCell(self._lstmParams.lstmSize())])
 
 			# generate prediction
-			dataset = tf.split(data, self._lstmParams.fftFreqBins(), 1)
+			print(data.shape)
+			dataset = tf.reshape(data, (-1, self._lstmParams.fftFreqBins()*2, 1))
+			print(dataset.shape)
+			dataset = tf.split(dataset, 5, 1)
+			print(dataset)
+
 			outputs, states = tf.contrib.rnn.static_rnn(rnn_cell, dataset, dtype=tf.float32)
 			# there are n_input outputs but
 			# we only want the last output
-			return tf.matmul(outputs[-1], self._weight_variable(
-				[self._lstmParams.lstmSize(), self._lstmParams.fftFreqBins()])) + self._bias_variable(
-				[self._lstmParams.fftFreqBins()])
+			output = tf.matmul(outputs[-1], self._weight_variable(
+				[self._lstmParams.lstmSize(), self._lstmParams.fftFreqBins()*2])) + self._bias_variable(
+				[self._lstmParams.fftFreqBins()*2])
+			return tf.reshape(output, [-1, self._lstmParams.fftFreqBins, 2])
 
 	def _weight_variable(self, shape):
 		return tf.get_variable('W', shape, initializer=tf.contrib.layers.xavier_initializer())
