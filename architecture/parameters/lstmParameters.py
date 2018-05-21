e766cf93d5f449321c9caba38d1a6a4d3dafe3ca
filsino <andimarafioti@gmail.com>
2018-05-21 16:37:45 +0200

    lstm parameters

---
+++ architecture/parameters/lstmParameters.py
@@ -0,0 +1,11 @@
+class LstmParameters(object):
+	def __init__(self, lstmSize, fftWindowLength, fftHopSize):
+		self._lstmSize = lstmSize
+		self._fftWindowLength = fftWindowLength
+		self._fftHopSize = fftHopSize
+
+	def lstmSize(self):
+		return self._lstmSize
+
+	def fftFreqBins(self):
+		return self._fftWindowLength//2+1
