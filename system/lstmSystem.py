6cb80240e973a06e1201f2cce268a92301b04878
filsino <andimarafioti@gmail.com>
2018-05-22 13:53:00 +0200

    Removed the conv layers and adapted the data to have every example be a batch

--- system/lstmSystem.py
+++ system/lstmSystem.py
@@ -10,7 +10,7 @@ class LSTMSystem(DNNSystem):
 		self._audio = tf.placeholder(tf.float32, shape=(batchSize, self._windowSize), name='audio_data')
 		self._inputAndTarget = aPreProcessor.inputAndTarget(self._audio)
 		super().__init__(architecture, name)
-		self._SNR = tf.reduce_mean(self._pavlovs_SNR(self._architecture.output(), self._architecture.target(), onAxis=[1,2]))
+		self._SNR = tf.reduce_mean(self._pavlovs_SNR(self._architecture.output(), self._architecture.target(), onAxis=[1]))
 
 	def optimizer(self, learningRate):
 		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
--- testArchitecture.py
+++ testArchitecture.py
@@ -3,15 +3,15 @@ from architecture.simpleLSTMArchitecture import SimpleLSTMArchitecture
 from system.lstmPreAndPostProcessor import LSTMPreAndPostProcessor
 from system.lstmSystem import LSTMSystem
 
-sessionsName = "LSTM_test_1"
+sessionsName = "LSTM_test_2_"
 
 params = LstmParameters(lstmSize=512, signalLength=5120, fftWindowLength=128, fftHopSize=32)
-batch_size = 64
+batch_size = 1
 
-aContextEncoderArchitecture = SimpleLSTMArchitecture(inputShape=(batch_size, 612, params.fftFreqBins(), 2), lstmParams=params)
+aContextEncoderArchitecture = SimpleLSTMArchitecture(inputShape=(batch_size, 612, params.fftFreqBins()), lstmParams=params)
 
 aPreProcessor = LSTMPreAndPostProcessor(params)
 
 aContextEncoderSystem = LSTMSystem(aContextEncoderArchitecture, batch_size, aPreProcessor, sessionsName)
 
-aContextEncoderSystem.train("nsynth_train_w5120_g1024_h512.tfrecords", "nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
+aContextEncoderSystem.train("fake_w5120_g1024_h512.tfrecords", "fake_w5120_g1024_h512.tfrecords", 1e-3)
