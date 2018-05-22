008f88b77a0fc1cbaccadb938960dcf31f9a315e
filsino <andimarafioti@gmail.com>
2018-05-22 16:27:46 +0200

    found the bug and crushed it

--- system/lstmSystem.py
+++ system/lstmSystem.py
@@ -28,16 +28,15 @@ class LSTMSystem(DNNSystem):
 
 			inputShape = list(STFT.shape)
 			outputShape = inputShape
-			outputShape[0] = inputShape[0] + length
+			outputShape[0] = self._lstmParameters.countOfFrames() + length
 			spectrograms = np.zeros(outputShape, dtype=np.float32)
-			spectrograms[:len(STFT)] = STFT
+			spectrograms[:self._lstmParameters.countOfFrames()] = STFT[:self._lstmParameters.countOfFrames()]
 			for i in range(length):
-				print(spectrograms.shape)
-				input_data = spectrograms[-self._lstmParameters.countOfFrames():]
+				next_frame = self._lstmParameters.countOfFrames() + i
+				input_data = spectrograms[next_frame-self._lstmParameters.countOfFrames():next_frame]
 				feed_dict = {self._architecture.testInput(): input_data, self._architecture.isTraining(): False}
 				nextSpectrogram = sess.run(self._architecture.generatedOutput(), feed_dict=feed_dict)
-				print(nextSpectrogram.shape)
-				spectrograms[len(STFT)+i] = nextSpectrogram
+				spectrograms[self._lstmParameters.countOfFrames()+i] = nextSpectrogram
 			return spectrograms
 
 	def optimizer(self, learningRate):
