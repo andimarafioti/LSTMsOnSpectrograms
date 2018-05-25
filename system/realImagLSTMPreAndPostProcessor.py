e4d2e294830a387743cb29504a58592a4dd08797
filsino <andimarafioti@gmail.com>
2018-05-25 15:21:32 +0200

    use real and Imag

--- system/realImagLSTMPreAndPostProcessor.py
+++ system/realImagLSTMPreAndPostProcessor.py
@@ -14,21 +14,10 @@ class RealImagLSTMPreAndPostProcessor(object):
 
 		return netInput, target
 
-		# netInput = np.empty([0, self._lstmParameters.fftFreqBins()])
-		# target = np.empty([0, self._lstmParameters.fftFreqBins()])
-		# frames_width = self._lstmParameters.countOfFrames()
-		#
-		# for step in range(realAndImagStft.shape.as_list()[-2]-frames_width-1):
-		# 	netInput = tf.concat([netInput, realAndImagStft[0, step:step + frames_width, :]], -2)
-		# 	target = tf.concat([target, realAndImagStft[0, step + frames_width + 1:step + frames_width + 2, :]], -2)
-		#
-		# return netInput, target
-
 	def _realAndImagStft(self, signal):
 		stft = tf.contrib.signal.stft(signals=signal,
 									  frame_length=self._lstmParameters.fftWindowLength(),
 									  frame_step=self._lstmParameters.fftHopSize())
-		return stft
-		# real_part = tf.real(stft)
-		# imag_part = tf.imag(stft)
-		# return tf.stack([real_part, imag_part], axis=-1, name='divideComplexIntoRealAndImag')
+		real_part = tf.real(stft)
+		imag_part = tf.imag(stft)
+		return tf.stack([real_part, imag_part], axis=-1, name='divideComplexIntoRealAndImag')
