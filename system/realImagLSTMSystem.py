7bdf4d35214619d6592804c237e272f03389c6a0
filsino <andimarafioti@gmail.com>
2018-05-25 15:48:50 +0200

    fixed images

--- system/realImagLSTMSystem.py
+++ system/realImagLSTMSystem.py
@@ -6,16 +6,19 @@ from utils.colorize import colorize
 class RealImagLSTMSystem(LSTMSystem):
 	def _spectrogramImageSummary(self):
 		frames = 12
-		originalAndGeneratedSpectrogram = tf.abs(self._architecture.generateXOutputs(self._architecture.input(), frames))
+		originalAndGeneratedSpectrogram = self._architecture.generateXOutputs(self._architecture.input(), frames)
+		originalAndGeneratedSpectrogram = tf.abs(
+			originalAndGeneratedSpectrogram[:, :, 0] + 1.0j * originalAndGeneratedSpectrogram[:, :, 1])
 		originalAndGeneratedSpectrogram = tf.transpose(originalAndGeneratedSpectrogram)
 		originalAndGeneratedSpectrogram = colorize(originalAndGeneratedSpectrogram)
 		originalAndGeneratedSpectrogram = tf.expand_dims(originalAndGeneratedSpectrogram, 0)
 
 		originalImage = originalAndGeneratedSpectrogram[:, :, int(self._lstmParameters.fftFrames()) -
-															frames-1:int(self._lstmParameters.fftFrames())-1, :]
-		generatedImage = originalAndGeneratedSpectrogram[:, :, int(self._lstmParameters.fftFrames())-1:, :]
+															  frames - 1:int(self._lstmParameters.fftFrames()) - 1, :]
+		generatedImage = originalAndGeneratedSpectrogram[:, :, int(self._lstmParameters.fftFrames()) - 1:, :]
 
 		return tf.summary.merge([tf.summary.image("Original", originalImage),
-								tf.summary.image("Generated", generatedImage),
-								tf.summary.image("Complete", originalAndGeneratedSpectrogram[
-															:, :, int(self._lstmParameters.fftFrames())-frames:, :])])
+								 tf.summary.image("Generated", generatedImage),
+								 tf.summary.image("Complete", originalAndGeneratedSpectrogram[
+															  :, :, int(self._lstmParameters.fftFrames()) - frames:,
+															  :])])
